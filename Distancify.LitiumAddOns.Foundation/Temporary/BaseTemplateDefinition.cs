1e6a38c6396ca96a6479514481949dd8b11817aa
Paul Birsan <paul@distancify.com>
2019-07-08 14:14:15 +0300

    Add temporary folder

---
+++ Distancify.LitiumAddOns.Foundation/Temporary/BaseTemplateDefinition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Distancify.Migrations.Litium.Seeds;
+using Litium.FieldFramework;
+using Litium.Runtime.AutoMapper;
+using Litium.Runtime.DependencyInjection;
+
+namespace Distancify.LitiumAddOns.CMS.Pages
+{
+    [Service(ServiceType = typeof(BaseTemplateDefinition))]
+    public abstract class BaseTemplateDefinition
+    {
+        public const string GeneralFieldGroupID = "General";
+
+        protected string GetTemplatePath(Type controllerType, string actionName)
+        {
+            return $"~/MVC:{controllerType.MapTo<string>()}:{actionName}";
+        }
+
+        protected FieldTemplateFieldGroup GetBareMinimumGeneralFieldTemplateFieldGroup()
+        {
+            return new FieldTemplateFieldGroup()
+            {
+                Id = GeneralFieldGroupID,
+                Collapsed = false,
+                Localizations =
+                {
+                    [Cultures.sv_SE] = {Name = "Allmänt"},
+                    [Cultures.en_US] = {Name = "General"},
+                },
+                Fields =
+                {
+                    SystemFieldDefinitionConstants.Name,
+                    SystemFieldDefinitionConstants.Url
+                }
+            };
+        }
+
+        public virtual IEnumerable<FieldDefinition> GetFieldDefinitions() => new List<FieldDefinition>();
+        public abstract IEnumerable<FieldTemplate> GetTemplates();
+    }
+}
