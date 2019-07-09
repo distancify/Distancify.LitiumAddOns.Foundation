528aa35631c0d430cac1e23700d08640d95ba52d
Paul Birsan <paul@distancify.com>
2019-07-09 09:25:48 +0300

    Get rid of the media mapper

--- Distancify.LitiumAddOns.Foundation/Foundation/ServiceRegistration.cs
+++ Distancify.LitiumAddOns.Foundation/Foundation/ServiceRegistration.cs
@@ -1,13 +1,12 @@
-﻿using System.Reflection;
+﻿using System.Configuration;
+using System.Reflection;
 using Distancify.LitiumAddOns.Foundation.Email;
-using Distancify.LitiumAddOns.Foundation.InversionOfControl;
+using Distancify.LitiumAddOns.MediaMapper;
+using Distancify.LitiumAddOns.MediaMapper.Services;
 using Distancify.LitiumAddOns.PIM.WorkflowAutomations;
-using Distancify.LitiumAddOns.ProductMedia.Mapping;
 using Distancify.LitiumAddOns.Tasks.Synchronization;
-using Distancify.LitiumAddOns.Wrappers.CMS;
-using Distancify.LitiumAddOns.Wrappers.MediaArchive;
-using Litium.Foundation.Modules.MediaArchive;
 using Litium.Owin.InversionOfControl;
+
 
 namespace Distancify.LitiumAddOns.Foundation
 {
@@ -16,10 +15,6 @@ namespace Distancify.LitiumAddOns.Foundation
         public void Install(IIoCContainer container, Assembly[] assemblies)
         {
             container.For<IAutomatedUserSetup>().ImplementedBy<AutomatedUserSetup>().RegisterAsScoped();
-
-            container.For<IMediaArchive>().ImplementedBy<MediaArchive>().RegisterAsScoped();
-            container.For<IMediaMapper>().ImplementedBy<MediaMapper>().RegisterAsScoped();
-
             container.For<IWorkflowAutomation>().RegisterAsScoped();
             container.For<IAutomationRunner>().ImplementedBy<ProductAutomationRunner>().RegisterAsScoped();
 
