6798f7a566f0c567696719c9922d70a4b11ca4da
Vlad Szabo <vlad@distancify.com>
2019-07-23 14:42:38 +0300

    Add website startup tasks for localization texts

---
+++ Distancify.LitiumAddOns.Foundation/StartupTasks/WebsiteStringsSetup.cs
@@ -0,0 +1,32 @@
+﻿using Distancify.LitiumAddOns.Foundation.Localization;
+using Litium;
+using Litium.Foundation;
+using Litium.Owin.Lifecycle;
+using Litium.Websites;
+
+namespace Distancify.LitiumAddOns.Foundation.StartupTasks
+{
+    public class WebsiteStringsSetup : IStartupTask
+    {
+        private readonly WebsiteService _websiteService;
+
+        public WebsiteStringsSetup(WebsiteService websiteService)
+        {
+            _websiteService = websiteService;
+        }
+
+        public void Start()
+        {
+            using (Solution.Instance.SystemToken.Use())
+            {
+                foreach (var website in _websiteService.GetAll())
+                {
+                    foreach (var definition in IoC.ResolveAll<WebsiteTextsDefinition>())
+                    {
+                        definition.Setup(website.SystemId, false);
+                    }
+                }
+            }
+        }
+    }
+}
